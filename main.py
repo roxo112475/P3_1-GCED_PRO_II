0ff213c5c29ab3afd3149a93ca17f3ebce3ad28c
Freezerole <momilbarreiro@gmail.com>
2025-04-23 19:36:42 +0200

    Main hecho, falta el formato de las cosas

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 from linked_ordered_positional_list import LinkedOrderedPositionalList as LOP
 from array_ordered_positional_list import ArrayOrderedPositionalList as AOP
-from clase_piezas import Piezas
 
-def read_orders(path="pedidos.txt") :
-    
-    """
-    read the car's orders
-
-    Attributes :
-    ------------
-        path
-    the txt where the progrem reads the data
-
-    Return :
-    --------
-        list
-    """
-    print()
-    print()
+def read_orders(path="pedidos.txt"):
     lista_pedidos = []
-    with open(path, encoding = 'utf8') as f:
+    with open(path) as f:
         for l in f.readlines():
             ls = l.strip().split(",")
             customer, model_name = ls[0], ls[1]
@@ -50,28 +34,14 @@ def procesar_pedidos(lista_pedidos:list):
 
     
             
-def read_parts(path="piezas.txt"):  #Abrir y leer el documento piezas, crear inventario
-    """
-    Read the whole part's inventary, name and number of parts.
-
-     Atributes :
-     -----------
-        path
-    path of the txt where the program read the data.
-
-    Return :
-    --------
-        inventario 
-    Array Ordered Positional List ; with the inventary
-    """
+def read_parts(path="piezas.txt"):  #Abrir y leer el documento piezas, crear inventario 
     inventario = AOP()
 
-    with open(path, encoding = 'utf8') as f:
+    with open(path) as f:
         for l in f.readlines():
             ls = l.strip().split(",")
             part_name, qty = ls[0], int(ls[1])
-
-            piezas = Piezas(part_name, qty)
+            piezas = [part_name, qty]
             inventario.add(piezas)
 
         return inventario
@@ -83,25 +53,21 @@ def read_models(path="modelos.txt") :
     """
     Read the car's parts and return the models with their own parts and number of parts.
 
-    Precondition: Models must be ordered at .txt, don't repit a model after another appeared.
-
     Atributes :
     -----------
         path
     File .txt where the program read the data.
     Return :
     --------
-        catalogo
-    dict; {model : (part, int number of parts)}
+        dict : {model : (part, int number of parts)}
     """
     catalogo = {}
-    with open(path, encoding = 'utf8') as f:
+    with open(path) as f:
         for l in f.readlines():
             ls = l.strip().split(",")
             model_name, part_name, qty = ls[0], ls[1], int(ls[2])
             piezas = (part_name, qty)
- 
-
+            
             if model_name not in catalogo.keys(): #Si el modelo aun no se ha registrado crea una LOP sobre la que añadir tuplas en vez de reescribirlas
                 catalogo[model_name] = LOP()
                 
@@ -109,11 +75,11 @@ def read_models(path="modelos.txt") :
     return catalogo
 
 
-def ensamblar(construccion: str, inventario):
+def ensamblar(construccion: str, inventario, catalogo):
     """Dado el nombre de un modelo, intenta ensamblarlo restando las piezas necesarias del inventario"""
-    if  isinstance(construccion, str):
+    if  construccion in catalogo.keys():
     
-        modelo = catalogo.get(construccion)
+        modelo = catalogo.get(construccion) #El coche en cuestion
     
         pos = modelo.first()  # Obtener la primera posición de la lista LOP
 
@@ -121,43 +87,47 @@ def ensamblar(construccion: str, inventario):
             pieza, cantidad = modelo.get_element(pos)
             pos = modelo.after(pos)
     
-            # Buscar la pieza en el inventario
+            # Buscar la pieza en el inventario de forma comparativa
             inv_pos = inventario.first()
+
             found = False
-            while inv_pos is not None:
+            while inv_pos is not None: #Parará después del ultimo (el after del ultimo sera None)
                 pieza_inv = inventario.get_element(inv_pos)
-                if pieza_inv.nombre == pieza:
+                if pieza_inv[0] == pieza: #Nombre pieza inventario
                     found = True
-                    if pieza_inv.cantidad >= cantidad:
-                        pieza_inv.cantidad -= cantidad
-                        print(f"{cantidad} unidades de '{pieza}' descontadas del inventario.")
+                    if pieza_inv[1] >= cantidad:
+                        pieza_inv[1] -= cantidad
+                        
                     else:
-                        print(f"No hay suficientes unidades de '{pieza}' para ensamblar {construccion}.")
-                    break
+                        print(f"No hay suficientes unidades de '{pieza}' para ensamblar el modelo {construccion}.")
+                        catalogo.pop(construccion)
+                        print(f"El modelo {construccion} ha sido eliminado del catálogo")                        
+                        return #Acaba la funcion tras detectar que falta al menos una pieza; elimina el coche del catalogo
+                    
                 inv_pos = inventario.after(inv_pos)
             
             if not found:
+                catalogo.pop(construccion)
                 print(f"Pieza '{pieza}' no encontrada en el inventario.")
+    
+            
 
 
 if __name__ == "__main__":
     lista_pedidos = read_orders()
+    inventario = read_parts() #array de partes disponibles
+    catalogo = read_models() # diccionario de automóviles disponibles
+    
     while len(lista_pedidos) > 0: 
-        ### 1 LEER Y MOSTRAR EL STOCK ###
-        inventario = read_parts() #array de partes disponibles
-        catalogo = read_models() # diccionario de automóviles disponibles
+
         construccion = procesar_pedidos(lista_pedidos)
-        ensamblar(construccion, inventario)
+        ensamblar(construccion, inventario, catalogo)
 
         print("---STOCK--- \n ")  #Imprimir el stock
         for elementos in inventario:
             print(*elementos)
         print()
-
-        ### 2 LECTURA DEL CATALOGO ###
-        catalogo = read_models() # diccionario de automóviles disponibles
-
-        ### 3 MOSTRAR EL CATALOGO ###
+        
         print("-----CATÁLOGO---- \n")    #Imprimir el catálogo
         for modelo, lista in catalogo.items():
             print("Modelo : ", modelo)
@@ -165,22 +135,4 @@ if __name__ == "__main__":
             print(f"{frase} \n" )
 
 
-
-        ### 4 PROCESAR EL FICHERO DE PEDIDOS ###
-        construccion = procesar_pedidos(lista_pedidos)
-
-        ### 5 MOSTRAR CATÁLOGO ACTUALIZADO CON CAMBIOS ###
-        # hasta que no se termine el paso 4 estará indicando lo mismo que en el 3
-        print("\n -----CATÁLOGO----")    #Imprimir el catálogo
-        for modelo, lista in catalogo.items():
-            print("Modelo : ", modelo)
-            frase = "| ".join(f"{pieza[0]}: {pieza[1]}" for pieza in lista)
-            print(f"{frase} \n" )
-
-
-
-        
-
-
-
-#Necesito una clase de piezas para poder buscar en la lista 
+ 
